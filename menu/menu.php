a027aefb13538b6d4c0a6d330953f72857baeba4
timoline <timoline@gmail.com>
2013-09-20 21:19:31 +0200

    some corrections

--- menu/menu.php
+++ menu/menu.php
@@ -5,6 +5,6 @@
 	<?php for ($i = 0; $i < (count($url)); $i++):?>
 	<?php $class="";?>	
 	<?php if ((isset($pagelink)) && ($pagelink == $page[$i])) { $class="active"; } ?>
-		<?php echo "<a class=\"list-group-item $class\"  href=$url[$i]><i class=\"glyphicon $icon[$i]\"></i> $title[$i]</a>"; ?>
+		<?php echo "<a class=\"list-group-item $class\"  href=.$url[$i]><i class=\"glyphicon $icon[$i]\"></i> $title[$i]</a>"; ?>
 	<?php endfor;?>	
 </div>
--- menu/menu_links.php
+++ menu/menu_links.php
@@ -5,13 +5,13 @@ $page			= array();
 $description	= array();
 $icon			= array();
 
-$url[] 			= "/synoportal/index.php?pagelink=intralinks";
+$url[] 			= "/index.php?pagelink=intralinks";
 $title[] 		= "Intralinks";
 $page[] 		= "intralinks";
 $description[] 	= "Intralinks";
 $icon[] 		= "glyphicon-home";
 
-$url[]			= "/synoportal/index.php?pagelink=favorites";
+$url[]			= "/index.php?pagelink=favorites";
 $title[]		= "Favorites";
 $page[] 		= "favorites";
 $description[]	= "Favorites";
