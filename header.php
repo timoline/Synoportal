bc9be0bbcb3b47098730ea993a02ed0994eda3f0
timoline <timoline@gmail.com>
2013-09-20 22:47:37 +0200

    fixed header name

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <div class="navbar navbar-default navbar-fixed-top">
   <div class="container">
 	<div class="navbar-header">
-	  <a class="navbar-brand" href="#">Synoportal</a>
+	  <a class="navbar-brand" href="#"><?php echo $config["sitename"];?></a>
 	</div>
   </div>
 </div>
