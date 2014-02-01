05c1ffc8a4e9e9d6d2acf74c61b68a2cc9ee68ba
timoline <timoline@gmail.com>
2014-02-01 21:26:08 +0100

    fixed Toggle navigation

--- index.php
+++ index.php
@@ -43,9 +43,11 @@ $settings = $db->getSettings();
 	<div class="container">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-			  <span class="sr-only">Toggle navigation</span>
-			  <span class="icon-bar"></span>
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
 			</button>
 			<a id="showMenu" class="navbar-brand" href="#"><?php echo $settings['sitename'];?></a>
 		</div>
