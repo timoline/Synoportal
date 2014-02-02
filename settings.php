e8929822238e1535f57e5b42a1f06aded137d7d4
timoline <timoline@gmail.com>
2014-02-02 19:17:03 +0100

    fixed viewport for mobile

--- settings.php
+++ settings.php
@@ -17,19 +17,30 @@
 	
 	$ippSelect = $gen->selector('ipp', $settings['ipp'], $ippArray);	
 ?>
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>SynoPortal - Settings</title>
-		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
-		<!-- css -->
-		<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">	
-		<link href="assets/css/style.css" rel="stylesheet" type="text/css">	
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" 
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="application-name" content="Synoportal">
+	<meta name="description" content="Web portal for the Synology NAS">
+	<meta name="author" content="Timoline">	
+	
+	<title>SynoPortal - Settings</title>
+	
+	<!-- Mobile settings -->
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
+	
+	<!-- css -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">	
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css">	
 	<!-- js -->
 	<script src="assets/js/jquery.js" type="text/javascript"></script>
 	<script src="assets/js/bootstrap.js" type="text/javascript"></script>	
 	<script src="assets/js/settings.js" type="text/javascript"></script>
+	
+	<!-- fav and touch icons -->
+	<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">		
 	</head>
 	<body class="settings">
 			
