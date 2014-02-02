6f38e9edf80641dc61aa0dc5c05243cea5bd5f60
timoline <timoline@gmail.com>
2014-02-02 08:23:09 +0100

    added logout to settings navbar

--- settings.php
+++ settings.php
@@ -63,6 +63,17 @@
 			</button>
 			<a class="navbar-brand" href="#"><?php echo $settings['sitename'];?></a>
 		</div>
+		  <!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="?logout=1">Logout</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->		
 	 </div>
 </nav>
 	<div id="containerSettings">
